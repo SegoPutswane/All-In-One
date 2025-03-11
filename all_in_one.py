206e9bce3772120cda8f6242b648d8c3bec0f35d
wtc <wtc@pop-os.localdomain>
2025-03-11 11:46:19 +0200

    fizzbuzz function done

--- all_in_one.py
+++ all_in_one.py
@@ -41,6 +41,17 @@ def fizzbuzz(n):
     TODO: define a function called fizzbuzz and implement the fucntionality above.
     """
 
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+    
+fizzbuzz(15)
 
 # Question 4
 def find_even_numbers(numbers):
